64f59aaf63856e3345861f056e15fc8fb4f527b9
Faisal Samudra <isalsamudra@hotmail.com>
2023-03-17 22:14:29 +0800

    Updated home page

--- src/Components/cardFeatures.jsx
+++ src/Components/cardFeatures.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
 
-const cardFeatures = () => {
+const CardFeatures = ({cardImg, cardTitle, cardDesc, route}) => {
   return (
-    <div>cardFeatures</div>
+    
+      <div classNameName="col">
+        <a href={`${route}`} className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow" style={{backgroundImage:`url(${cardImg})`}}>
+        <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-2">
+            <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{cardTitle}</h3>
+            <ul class="d-flex list-unstyled mt-auto">
+              <li class="d-flex align-items-center me-3">
+                <div>image here</div>
+                <small>Earth</small>
+              </li>
+              <li class="d-flex align-items-center">
+                <div>image here</div>
+                <small>3d</small>
+              </li>
+            </ul>
+          </div>
+        </a>
+      </div>
+    
+
+      
   )
 }
 
-export default cardFeatures
+export default CardFeatures
--- src/Views/home.jsx
+++ src/Views/home.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
-import { useEffect } from 'react';
-import Hero from '../Components/hero';
-import FeaturedDatasets from '../Components/featuredDatasets';
+import React from "react";
+import { useEffect } from "react";
+import Hero from "../Components/hero";
+import FeaturedDatasets from "../Components/featuredDatasets";
+import CardFeatures from "../Components/cardFeatures";
+import HdbCardImg from "../assets/hdb-card.jpg";
+import PpCardImg from "../assets/pp-card.jpg";
 
 const Home = () => {
-
   useEffect(() => {
-    document.title = 'Lofty | Home';
+    document.title = "Lofty | Home";
   }, []);
+
+  const cardData = [
+    [
+      HdbCardImg,
+      "HDB Housing Analytics",
+      "Compare prices, resale volume of flats and ammenities",
+      "/hdb",
+    ],
+    [
+      PpCardImg,
+      "Private Property Analytics",
+      "Compare prices, resale volume of flats and ammenities",
+      "/private_property",
+    ],
+  ];
 
   return (
     <>
-      <Hero/>
-      <FeaturedDatasets/>
+      <Hero />
+      <FeaturedDatasets />
+      <div className="container px-4 py-5">
+        <h2 className="pb-2">
+          Discover your dream property with analytics
+        </h2>
+        <div className="row row-cols-1 row-cols-lg-2 align-items-stretch g-4 py-5">
+          {cardData.map((card, index) => {
+            return (
+              <CardFeatures
+                cardImg={card[0]}
+                cardTitle={card[1]}
+                cardDesc={card[2]}
+                route={card[3]}
+              />
+            );
+          })}
+        </div>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
