1ee5bd7e19097072382ba2f93e8aba6e7c42a42b
Faisal Samudra <isalsamudra@hotmail.com>
2023-03-17 21:11:28 +0800

    Updated home page

--- src/Components/featuredDatasets.jsx
+++ src/Components/featuredDatasets.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import DataGovLogo from '../assets/data-gov-logo.png'
+import HdbLogo from '../assets/hdb-logo-full.png'
+import UraLogo from '../assets/ura-logo-full.png'
+import LtaLogo from '../assets/lta-logo.png'
+
 
 
 const FeaturedDatasets = () => {
   return (
     <div className="container">
-        <div className="text-center">Lofty uses combination of datasets and API</div>
-        <div className="row">
-            <div className="col-4"></div>
+        <div className="d-flex justify-content-center">Lofty uses combination of datasets and API</div>
+        <div className="d-flex-column d-lg-flex justify-content-center py-3">
+          <div className="px-5 py-3 text-center "><img className="datasets-logo" src={HdbLogo}/></div>
+          <div className="px-5 py-3 text-center"><img className="datasets-logo" src={UraLogo}/></div>
+          <div className="px-5 py-3 text-center"><img className="datasets-logo" src={DataGovLogo}/></div>
+          <div className="px-5 py-3 text-center"><img className="datasets-logo" src={LtaLogo}/></div>
         </div>
+        
+
     </div>
   )
 }
