01ceec23363389e18545e6accdf352dfe32b122a
MUKMIN PITOYO <mpitoyo.2020@scis.smu.edu.sg>
2023-03-29 15:34:46 +0800

    comment out

--- server/app/hdb.py
+++ server/app/hdb.py
@@ -143,53 +143,53 @@ def get_lease_data():
                 lease_bins["59-55 years"].append(doc)
 
         output_data = [lease_bins]
-        temp_dict={}
+        # temp_dict={}
 
-        for key in lease_bins:
-            print("Doing lease bin: " + key)
-            print(len(lease_bins[key]))
+        # for key in lease_bins:
+        #     print("Doing lease bin: " + key)
+        #     print(len(lease_bins[key]))
 
-            temp_resale_dict = {}
-            for flat_type in flat_types:
-                print(flat_type)
-                count = 0
-                total_resale_price = 0
-                for data in lease_bins[key]:
-                    if data['flat_type'] == flat_type:
-                        total_resale_price += int(data['resale_price'])
-                        # CODE STOPS WORKING HERE, 94-90 YEARS 5 ROOM FLAT
-                        count+=1
-                        print(total_resale_price, count)
-                if count > 0:
-                    avg_resale_price = total_resale_price / count
-                    temp_resale_dict[flat_type] = avg_resale_price
-                    print("The average resale value for flat type " + flat_type + " is " + avg_resale_price)
-                else:
-                    temp_resale_dict[flat_type] = 0
-                    print("No resale houses for flat type: " + flat_type)
+        #     temp_resale_dict = {}
+        #     for flat_type in flat_types:
+        #         print(flat_type)
+        #         count = 0
+        #         total_resale_price = 0
+        #         for data in lease_bins[key]:
+        #             if data['flat_type'] == flat_type:
+        #                 total_resale_price += int(data['resale_price'])
+        #                 # CODE STOPS WORKING HERE, 94-90 YEARS 5 ROOM FLAT
+        #                 count+=1
+        #                 print(total_resale_price, count)
+        #         if count > 0:
+        #             avg_resale_price = total_resale_price / count
+        #             temp_resale_dict[flat_type] = avg_resale_price
+        #             print("The average resale value for flat type " + flat_type + " is " + avg_resale_price)
+        #         else:
+        #             temp_resale_dict[flat_type] = 0
+        #             print("No resale houses for flat type: " + flat_type)
 
-            temp_dict = {
-                "Lease Bins": key,
-                "EXECUTIVE": temp_resale_dict['EXECUTIVE'],
-                "EXECUTIVEColor": "hsl(293, 70%, 50%)",
-                "MULTI-GENERATION": temp_resale_dict['MULTI-GENERATION'],
-                "MULTI-GENERATION": "hsl(293, 70%, 50%)",
-                "5 ROOM": temp_resale_dict['5 ROOM'],
-                "5 ROOMColor": "hsl(285, 70%, 50%)",
-                "4 ROOM": temp_resale_dict['4 ROOM'],
-                "4 ROOMColor": "hsl(10, 70%, 50%)",
-                "3 ROOM": temp_resale_dict["3 ROOM"],
-                "3 ROOMColor": "hsl(241, 70%, 50%)",
-                "2 ROOM": temp_resale_dict["2 ROOM"],
-                "2 ROOMColor": "hsl(259, 70%, 50%)",
-                "1 ROOM": temp_resale_dict["1 ROOM"],
-                "1 ROOMColor": "hsl(270, 70%, 50%)"
-            }
-            print(temp_dict)
+        #     temp_dict = {
+        #         "Lease Bins": key,
+        #         "EXECUTIVE": temp_resale_dict['EXECUTIVE'],
+        #         "EXECUTIVEColor": "hsl(293, 70%, 50%)",
+        #         "MULTI-GENERATION": temp_resale_dict['MULTI-GENERATION'],
+        #         "MULTI-GENERATION": "hsl(293, 70%, 50%)",
+        #         "5 ROOM": temp_resale_dict['5 ROOM'],
+        #         "5 ROOMColor": "hsl(285, 70%, 50%)",
+        #         "4 ROOM": temp_resale_dict['4 ROOM'],
+        #         "4 ROOMColor": "hsl(10, 70%, 50%)",
+        #         "3 ROOM": temp_resale_dict["3 ROOM"],
+        #         "3 ROOMColor": "hsl(241, 70%, 50%)",
+        #         "2 ROOM": temp_resale_dict["2 ROOM"],
+        #         "2 ROOMColor": "hsl(259, 70%, 50%)",
+        #         "1 ROOM": temp_resale_dict["1 ROOM"],
+        #         "1 ROOMColor": "hsl(270, 70%, 50%)"
+        #     }
+        #     print(temp_dict)
 
-            output_data.append(temp_dict)
+        #     output_data.append(temp_dict)
         
-        print(lease_bins["94-90 years"][0])
+        # print(lease_bins["94-90 years"][0])
         
         response = {"Result": [output_data]}
 
