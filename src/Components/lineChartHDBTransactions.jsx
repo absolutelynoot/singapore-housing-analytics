715cc5e9fdf0193d84e2a194cf76b72734489731
jonathantan12 <74011226+jonathantan12@users.noreply.github.com>
2023-03-29 22:14:58 +0800

    line chart with filter

--- src/Components/lineChartHDBTransactions.jsx
+++ src/Components/lineChartHDBTransactions.jsx
@@ -2,17 +2,23 @@
 import { ResponsiveLine } from '@nivo/line'
 // import { data } from "./sample-data/line-chart-sample-data";
 import { useState, useEffect } from "react";
-import Slider from 'rc-slider'
-import 'rc-slider/assets/index.css'
+import Slider from '@mui/material/Slider';
+import Button from '@mui/material/Button';
 
 // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
 // no chart will be rendered.
 // website examples showcase many properties,
 // you'll often use just a few of them.
+
+function valuetext(value) {
+    return `${value}`;
+};
+
 const MyResponsiveLine = () => {
     const [data, setData] = useState([])
-    const [year, setYear] = useState('2023')
+    const [value, setValue] = useState([2017, 2023]);
+    // const [originalData, setOriginalData] = useState([])
 
     const handleFetchData = async () => {
         const response = await fetch('http://127.0.0.1:5000/hdb/total_transactions_over_months');
@@ -20,6 +26,7 @@ const MyResponsiveLine = () => {
             const temp = await response.json();
             // console.log(temp);
             setData(temp);
+            // setOriginalData(temp);
         }
     }
     
@@ -27,25 +34,65 @@ const MyResponsiveLine = () => {
         handleFetchData();
     },[])
 
-    const handleSliderChange = (value) => {
-        console.log(value);
-        setYear(value.toString())
-      }
+    const handleChange = (event, newValue) => {
+        // console.log(newValue);
+        setValue(newValue);
+        
+        // console.log(originalData);
+        // setData(originalData);
+
+        // console.log(data[0].data);
+
+        const result = data[0].data;
+
+        // List of dictionary, filter for x values
+        const selected = [];
+        for (var i = value[0]; i <= value[1]; i++) {
+            for (var j = 1; j <= 12; j++) {
+                if (j < 10) {
+                    selected.push(i.toString() + "-0" + j.toString());
+                }
+                else {
+                    selected.push(i.toString() + "-" + j.toString());
+                }
+            }
+        };
+        
+        // console.log(selected);
+        const res = result.filter(({ x }) => selected.includes(x));
+
+        // console.log(res);
+        
+        data[0].data = res;
+        setData(data);
+
+    };
+
+    const reset = () => {
+        console.log("reset");
+        setValue([2017, 2023]);
+        handleFetchData();
+        // handleChange();
+    };
 
     return (
         <div style={{height:"600px", marginBottom:"200px"}}>
+            <div className="title">Filter based on Years</div>
             <Slider
+                getAriaLabel={() => 'Year Range'}
+                value={value}
                 min={2017}
                 max={2023}
-                defaultValue={2023}
-                onChange={handleSliderChange}
-                // aria-label="Custom marks"
-                aria-label="Custom marks"
-                getAriaValueText={year}
+                onChange={handleChange}
                 valueLabelDisplay="auto"
-                step={1}
-                marks
+                getAriaValueText={valuetext}
             />
+            <Button
+            onClick={() => {reset()}}
+            >
+            Reset
+            </Button>
+
             <h1>Total Transactions vs Months</h1>
             <ResponsiveLine
                 data={data}
@@ -113,7 +160,7 @@ const MyResponsiveLine = () => {
                 ]}
             />
         </div>
-    )
+    );
 };
 
-export default MyResponsiveLine
+export default MyResponsiveLine;
