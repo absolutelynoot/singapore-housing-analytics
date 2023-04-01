beacac73a609be246cac82c6086a6d33bf5a5745
Faisal Samudra <isalsamudra@hotmail.com>
2023-04-01 12:33:33 +0800

    Added tableau cloropleth component

--- src/DashboardViews/Location.jsx
+++ src/DashboardViews/Location.jsx
@@ -1,10 +1,21 @@
-import SimpleMap from "../Components/geo"
-
+import SimpleMap from "../Components/geo";
+import CloroplethTableau from "../Components/CloroplethTableau";
 
 const Location = () => {
   return (
-      <SimpleMap/>
-  )
-}
+    <>
+      <div class="mb4">
+        <h1 class="mb-3">Location Analysis (Interactive)</h1>
+        <SimpleMap />
+      </div>
 
-export default Location
+      <div class="mb4">
+        <h1 class="mb-3">Average Resale Price Cloropleth Map</h1>
+        <CloroplethTableau />
+      </div>
+
+    </>
+  );
+};
+
+export default Location;
